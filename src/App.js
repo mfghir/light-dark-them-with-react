b874f375c676bf110347cfd1f90b6aabe44b1aa7
miss-f-g <fatemeghafari98@gmail.com>
2022-07-06 16:18:50 +0430

    light-dark them with react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Themechanger from "./components/Themechanger";
+
+function App() {
+  return (
+    <>
+      <Themechanger />
+      <div className="item input__container">
+        <input type="text" placeholder="Search..." />
+      </div>
+      <Dashboard />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Dashboard.js
@@ -0,0 +1,56 @@
+import "./Dashboard.css";
+
+const Dashboard = () => {
+  return (
+    <section>
+      <h2>My article</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+        tempore consequuntur voluptas maiores asperiores eligendi quo totam
+        labore quia? Ab neque tenetur dignissimos nesciunt consectetur, officia
+        sequi. Reiciendis, fuga sapiente!
+        <br />
+        <br />
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit beatae
+        nostrum, quas dolorum iste officia nesciunt minus assumenda aspernatur
+        consequuntur reprehenderit adipisci nisi obcaecati ea laborum. Est
+        assumenda similique itaque?
+      </p>
+
+      <h3>More stuff</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti porro
+        veniam cumque quod ipsum pariatur odit ducimus sunt, voluptate ratione
+        minima, itaque inventore quas libero nesciunt alias voluptas ipsa iure.
+        <br />
+        <br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ratione
+        temporibus ab vel, optio sunt quam velit alias explicabo hic ipsa
+        facilis perferendis autem aut perspiciatis exercitationem debitis
+        praesentium itaque. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Laborum ratione temporibus ab vel, optio sunt quam velit alias
+        explicabo hic ipsa facilis perferendis autem aut perspiciatis
+        exercitationem debitis praesentium itaque.
+      </p>
+
+      <h3>The last bit...</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti porro
+        veniam cumque quod ipsum pariatur odit ducimus sunt, voluptate ratione
+        minima, itaque inventore quas libero nesciunt alias voluptas ipsa iure.
+        <br />
+        <br />
+        <br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ratione
+        temporibus ab vel, optio sunt quam velit alias explicabo hic ipsa
+        facilis perferendis autem aut perspiciatis exercitationem debitis
+        praesentium itaque. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Laborum ratione temporibus ab vel, optio sunt quam velit alias
+        explicabo hic ipsa facilis perferendis autem aut perspiciatis
+        exercitationem debitis praesentium itaque.
+      </p>
+    </section>
+  );
+};
+
+export default Dashboard;
